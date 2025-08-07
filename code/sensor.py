38e45c24cedec8e40c83c7d5e6ca8e1bb795202d
wfairmanOlin <wfairman@olin.edu>
2025-08-07 15:28:07 -0400

    example code for bno055

--- code/sensor.py
+++ code/sensor.py
@@ -170,3 +170,18 @@ class I2CReader(QThread):
 
     def abort(self):
         self._abort = True
+
+if __name__ == "__main__":
+    i2c = board.I2C()
+    sensor = adafruit_bno055.BNO055_I2C(i2c)
+    while True:
+        print(f"Accelerometer (m/s^2): {sensor.acceleration}")
+        print(f"Magnetometer (microteslas): {sensor.magnetic}")
+        print(f"Gyroscope (rad/sec): {sensor.gyro}")
+        print(f"Euler angle: {sensor.euler}")
+        print(f"Quaternion: {sensor.quaternion}")
+        print(f"Linear acceleration (m/s^2): {sensor.linear_acceleration}")
+        print(f"Gravity (m/s^2): {sensor.gravity}")
+        print()
+
+        time.sleep(1)
